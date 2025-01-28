1ac65e4ab65e9fa28fa297a107ef8956d20e37ee
Wabelbit <140746618+Wabelbit@users.noreply.github.com>
2025-01-28 23:04:59 +0100

    Unzip selected zip file

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 const loadMauttabelle = async function (file) {
-    const reader = new FileReader();
-    reader.onload = async function (e) {
-        await unzipFile(e.target.result);
-    };
-    reader.readAsArrayBuffer(file)
+    const excelData = await unzipFile(file)
 }
 
 const unzipFile = async function (file) {
-    // TODO
+    const entries = await (new zip.ZipReader(new zip.BlobReader(file))).getEntries("utf-8");
+    console.debug("entries", entries)
+    const excelFiles = entries.filter(entry => entry.filename.endsWith(".xlsx"));
+    if (excelFiles.length !== 1) {
+        throw "Die ZIP Datei muss exakt 1 Excel-Datei enthalten, aber es gibt " + excelFiles.length;
+    }
+    const progressElem = document.getElementById("unzip-progress");
+    progressElem.hidden = false
+    const data = await excelFiles[0].getData(new zip.BlobWriter(), {
+        onprogress: onUnzipProgress
+    });
+    console.debug("data", data);
+    return data
+}
+
+const onUnzipProgress = function (index, max) {
+    progressElem.innerText = `${max / 1000} KB, ${(index / max) * 100}% geladen`;
 }
